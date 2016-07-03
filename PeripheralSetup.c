f242bad5a0e99bcd576b4dcb42dd20d3ea7818f5
josefco18 <jfmartinez18@hotmail.com>
2016-07-02 23:39:25 -0700

    SensorRead function added
    Adds a new function to the library to be able to read the voltage values
    for the different peripheral devices.

--- PeripheralSetup.c
+++ PeripheralSetup.c
@@ -9,6 +9,9 @@
 #include "stm32f4xx_adc.h"
 
 void SqueezeInit(){
+	GPIO_InitTypeDef GPIO_InitStruct;
+	ADC_InitTypeDef ADC_InitStruct;
+
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN,ENABLE);
 
@@ -36,6 +39,9 @@ void SqueezeInit(){
 
 
 void TapInit(){
+	GPIO_InitTypeDef GPIO_InitStruct;
+	ADC_InitTypeDef ADC_InitStruct;
+
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN,ENABLE);
 
@@ -63,6 +69,9 @@ void TapInit(){
 
 
 void SpinInit(){
+	GPIO_InitTypeDef GPIO_InitStruct;
+	ADC_InitTypeDef ADC_InitStruct;
+
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN,ENABLE);
 
@@ -89,22 +98,54 @@ void SpinInit(){
 }
 
 void BoardLEDInit (){
-RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-GPIO_InitStruct.GPIO_Pin= GPIO_Pin_15 | GPIO_Pin_14 | GPIO_Pin_13
-		| GPIO_Pin_12;
-GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-GPIO_Init(GPIOD, &GPIO_InitStruct);
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	GPIO_InitStruct.GPIO_Pin= GPIO_Pin_15 | GPIO_Pin_14 | GPIO_Pin_13
+			| GPIO_Pin_12;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
 }
 
 void PushButtonInit() {
-RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-GPIO_InitStruct.GPIO_Pin= GPIO_Pin_0;
-GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
-GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
-GPIO_Init(GPIOA, &GPIO_InitStruct);
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	GPIO_InitStruct.GPIO_Pin= GPIO_Pin_0;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+int SensorRead(char sensor){
+
+	if (sensor == 'squeeze'){
+		ADC_SoftwareStartConv(ADC1);
+		while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC)){
+
+		}
+			return ADC_GetConversionValue(ADC1);
+		}
+
+	if (sensor == 'tap'){
+		ADC_SoftwareStartConv(ADC2);
+		while(!ADC_GetFlagStatus(ADC2, ADC_FLAG_EOC)){
+
+		}
+			return ADC_GetConversionValue(ADC2);
+		}
+
+	if (sensor == 'spin'){
+		ADC_SoftwareStartConv(ADC3);
+		while(!ADC_GetFlagStatus(ADC3, ADC_FLAG_EOC)){
+
+		}
+			return ADC_GetConversionValue(ADC3);
+		}
+
 }
