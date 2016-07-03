68308d5ea2903efcdbd22e61c18c3f60de8c7ae0
josefco18 <jfmartinez18@hotmail.com>
2016-07-03 13:30:19 -0700

    Added delay function
    This way you can create delays in your main code

--- main.c
+++ main.c
@@ -4,77 +4,10 @@
 #include "stm32f4xx_adc.h"
 #include "PeripheralSetup.h"
 
-GPIO_InitTypeDef GPIO_InitStruct;
-ADC_InitTypeDef ADC_InitStruct;
 
-int Reading = 0;
-
-void initialize(){
-
-RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-GPIO_InitStruct.GPIO_Pin= GPIO_Pin_13 | GPIO_Pin_14;
-GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-GPIO_Init(GPIOD, &GPIO_InitStruct);
-
-RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-GPIO_InitStruct.GPIO_Pin= GPIO_Pin_0;
-GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
-GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
-GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-//	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-//	GPIO_InitStruct.GPIO_Pin= GPIO_Pin_1;
-//	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AN;
-//	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-//	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-//	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
-//	GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-
-
-RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
-RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN,ENABLE);
-
-//Analog Pin Config
-GPIO_InitStruct.GPIO_Pin= GPIO_Pin_0;
-GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AN;
-GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-ADC_DeInit();
-
-ADC_InitStruct.ADC_Resolution = ADC_Resolution_12b;
-ADC_InitStruct.ADC_ScanConvMode = DISABLE;
-ADC_InitStruct.ADC_ContinuousConvMode = ENABLE;
-ADC_InitStruct.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
-ADC_InitStruct.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
-ADC_InitStruct.ADC_DataAlign = ADC_DataAlign_Right;
-ADC_InitStruct.ADC_NbrOfConversion = 1;
-ADC_Init(ADC1, &ADC_InitStruct);
-
-ADC_Cmd(ADC1, ENABLE);
-
-ADC_RegularChannelConfig(ADC1,ADC_Channel_10,1,ADC_SampleTime_144Cycles);
-
-}
-
-int adc_convert(){
-	ADC_SoftwareStartConv(ADC1);
-	while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC)){
-
-	}
-		return ADC_GetConversionValue(ADC1);
-}
 int main(void)
 {
 	SysTick_Init();
-
-	initialize();
 
 	double Reading;
 	while (1){
