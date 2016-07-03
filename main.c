5a7a1841e3a8fc094a6a0d5bf522da68459785ae
josefco18 <jfmartinez18@hotmail.com>
2016-07-03 15:20:35 -0700

    Setup for Game Modes
    Began writing the main.c code to create the framework of how everything
    will function together

--- main.c
+++ main.c
@@ -3,25 +3,60 @@
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_adc.h"
 #include "PeripheralSetup.h"
+#include "delay.h"
 
+SqueezeInit(); //Connect Sensor to PC0
+TapInit(); //Connect Sensor to PC1
+SpinInit(); //Connect Sensor to PC2
+
+SqueezeLEDInit(); //Connect LED to PA0
+PushButtonInit (); //Turns on the blue push button on the stm board
 
 int main(void)
 {
+	//Initialize the delay timer
 	SysTick_Init();
 
-	double Reading;
-	while (1){
-		int state=GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0);
+	//Initialize values for each sensor reading
+	int SqueezeReading;
+	int TapReading;
+	int SpinReading;
 
+	//Initialize state of the blue button to OFF
+	int ButtonState=0;
+
+	//Begin main loop
+	while (1){
+		//Read the state of the blue button
+		ButtonState=GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0);
+
+		//If blue button is off, initialize the Free Play mode
 		if (!state){
-			GPIO_ToggleBits(GPIOD, GPIO_Pin_13 | GPIO_Pin_14);
+			//Turn on the LEDs to indicate it's in Free Play Mode
+			GPIO_SetBits(GPIOD, GPIO_Pin_15 | GPIO_Pin_14 | GPIO_Pin_13 | GPIO_Pin_12);
+
+			//Read the voltage values for each of the three peripherals
+			SqueezeReading=SensorRead("Squeeze");
+			TapReading=SensorRead("Tap");
+			SqueezeReading=SensorRead("Spin");
+
+			//
+
+
+
+
+
+
 			delay_1ms(500);
 		}
+
+		//If blue button is on, initialize Simon Says Mode
 		else{
-			GPIO_ResetBits(GPIOD, GPIO_Pin_13 | GPIO_Pin_14);
+			//Flash the LEDs ON and OFF to indicate Simon Says Mode
+			GPIO_ToggleBits(GPIOD, GPIO_Pin_15 | GPIO_Pin_14 | GPIO_Pin_13 | GPIO_Pin_12);
 		}
 
-		Reading=adc_convert();
+
 
 		printf("%d\r\n", Reading);
 
