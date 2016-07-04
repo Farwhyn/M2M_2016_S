da3096c5daf6b033078073b2481b07a63aaee201
josefco18 <jfmartinez18@hotmail.com>
2016-07-03 22:49:59 -0700

    Added Free Play Mode Framework
    Still requires LED strip and speaker code

--- main.c
+++ main.c
@@ -25,10 +25,16 @@ int main(void)
 	//Initialize state of the blue button to OFF
 	int ButtonState=0;
 
+	//Initialize Sensor Thresholds
+	int SqueezeThreshold=0;
+	int TapThreshold=0;
+	int SpinThreshold=0;
 	//Begin main loop
 	while (1){
 		//Read the state of the blue button
 		ButtonState=GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0);
+
+
 
 		//If blue button is off, initialize the Free Play mode
 		if (!state){
@@ -40,13 +46,20 @@ int main(void)
 			TapReading=SensorRead("Tap");
 			SqueezeReading=SensorRead("Spin");
 
-			//
+			if (SqueezeReading>SqueezeThreshold){
+				//Turn on Squeeze LED
+				//Play Squeeze music note
+			}
 
+			if (SpinReading>SqueezeThreshold){
+				//Turn on Spin LED
+				//Play Spin music note
+			}
 
-
-
-
-
+			if (TapReading>SqueezeThreshold){
+				//Turn on Tap LED
+				//Play Tap music note
+			}
 			delay_1ms(500);
 		}
 
@@ -54,6 +67,7 @@ int main(void)
 		else{
 			//Flash the LEDs ON and OFF to indicate Simon Says Mode
 			GPIO_ToggleBits(GPIOD, GPIO_Pin_15 | GPIO_Pin_14 | GPIO_Pin_13 | GPIO_Pin_12);
+
 		}
 
 
